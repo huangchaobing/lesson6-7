a7e227e500c726096ae4d7fac26f676379ef47e4
huangchaobing <715623229@qq.com>
2018-10-14 23:31:10 +0800

    提交代码

---
+++ zuulRatelimit/zuulRatelimit/src/test/java/com/software/apiGateway/ZuulRatelimitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.software.apiGateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulRatelimitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
